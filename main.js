20793e0efdad3b14259adfe7836d7b6349a739ce
Marilou <81520920+N4M1@users.noreply.github.com>
2023-02-20 11:47:51 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('main starting');
+const a = require('./a.js');
+const b = require('./b.js');
+console.log('in main, a.done = %j, b.done = %j', a.done, b.done);
+
+(function(exports, require, module, __filename, __dirname) {
+    // Module code actually lives in here
+    });
